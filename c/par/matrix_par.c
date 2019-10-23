d56252014343fe9ff67325a92d372727ea69da99
Roman de Prog <39725794+RomanKameniev@users.noreply.github.com>
2019-10-24 02:06:50 +0300

    updated c paralell algoritm

--- c/par/matrix_par.c
+++ c/par/matrix_par.c
@@ -2,11 +2,10 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdint.h>
-//#define MAX 2048 
-
-#define MAX 1024 
+#define MAX 1024
 #define RAND_KEY 100
 //comand for parralel compile - >gcc -Wall -Wextra -fopenmp
+
 clock_t start, finish;
 int64_t a[MAX][MAX], b[MAX][MAX], c[MAX][MAX];
 
@@ -28,7 +27,7 @@ int64_t power(int64_t a, int64_t b)
     return a * b;
 }
 
-double calculate()
+double calculate_ijk()
 {
     start = clock();
     #pragma omp parallel
@@ -48,12 +47,51 @@ double calculate()
             }
         }
     }
+
     finish = clock();
     return (double)(finish - start) / CLOCKS_PER_SEC;
 }
+
+double calculate_jik()
+{
+    start = clock();
+      #pragma omp parallel
+    {
+        int i, j, k;
+        #pragma omp for 
+        for (j = 0; j < MAX; j++)
+        {
+            for (i = 0; i < MAX; i++)
+            {
+                int64_t temp = 0;
+                for (k = 0; k < MAX; k++)
+                {
+                    temp += power(a[i][k], b[k][j]);
+                }
+                c[i][j] = temp;
+            }
+        }
+    }
+    finish = clock();
+    return (double)(finish - start) / CLOCKS_PER_SEC;
+}
+
+
+void emptyArr(int64_t arr[MAX][MAX])
+{
+    for (int i = 0; i < MAX; i++)
+    {
+        for (int j = 0; j < MAX; j++)
+        {
+            arr[i][j] = 0;
+        }
+    }
+}
+
 int main()
 {
     printf("C par program \n");
+
     printf("default value: \n arr length= %d \n key range from 0 to %d \n ", MAX, RAND_KEY);
     for (int i = 1; i <= 10; i++)
     {
@@ -61,8 +99,13 @@ int main()
         generateArr(a);
         generateArr(b);
         printf("calculating arr\n");
-        double calc = calculate();
-        printf("time of %d iteration is = %f \n test item of arr=> %I64ld \n", i, calc, c[0][0]);
+        double ijk_time = calculate_ijk();
+        printf(" ijk par time of %d iteration is = %lg \n test item of arr=> %I64ld \n", i, ijk_time, c[0][0]);
+        emptyArr(c);
+        double jik_time = calculate_jik();
+        printf(" jki par reverse => time of %d iteration is = %lg \n test item of arr=> %I64ld \n", i, jik_time, c[0][0]);
+        emptyArr(c);
+    
     }
     int key;
     printf("End \n");
