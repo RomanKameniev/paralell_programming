e371a365194eee77d21ef1097de58c7efee77766
Roman de Prog <39725794+RomanKameniev@users.noreply.github.com>
2019-10-24 03:56:39 +0300

    updated go algoritm

--- go/par/matrix_par.go
+++ go/par/matrix_par.go
@@ -11,7 +11,7 @@ const MAX int = 1024
 const RAND int64 = 100
 
 func main() {
-	fmt.Println("Go par program")
+	fmt.Println("Go seq program")
 	fmt.Println("default values : \n MAX = ", MAX, " \n RAND = ", RAND)
 	var a [MAX][MAX]int64
 	var b [MAX][MAX]int64
@@ -22,12 +22,25 @@ func main() {
 		generate(&a)
 		generate(&b)
 		fmt.Println("calculating")
-		var stamp time.Time = calculate(&a, &b, &c)
-		fmt.Println("time of ", iteration, " iteration is = ", time.Since(stamp), "\n test item of arr=> ", c[0][0])
+		var stamp_ijk time.Time = calculate_ijk(&a, &b, &c)
+		fmt.Println("time of ijk", iteration, " iteration is = ", time.Since(stamp_ijk), "\n test item of arr=> ", c[0][0])
+		empyarr(&c)
+		var stamp_jik time.Time = calculate_jik(&a, &b, &c)
+		fmt.Println("time of jik", iteration, " iteration is = ", time.Since(stamp_jik), "\n test item of arr=> ", c[0][0])
+		empyarr(&c)
+
 	}
 	var key int
 	fmt.Println("End ")
 	fmt.Scan(&key)
+}
+
+func empyarr(arr *[MAX][MAX]int64) {
+	for x := 0; x < MAX; x++ {
+		for y := 0; y < MAX; y++ {
+			arr[x][y] = 0
+		}
+	}
 }
 
 func generate(arr *[MAX][MAX]int64) {
@@ -44,7 +57,7 @@ func power(a int64, b int64) int64 {
 	return a * b
 }
 
-func calculate(arr1 *[MAX][MAX]int64, arr2 *[MAX][MAX]int64, arr3 *[MAX][MAX]int64) time.Time {
+func calculate_ijk(arr1 *[MAX][MAX]int64, arr2 *[MAX][MAX]int64, arr3 *[MAX][MAX]int64) time.Time {
 	start := time.Now()
 	var wg sync.WaitGroup
 	wg.Add(MAX)
@@ -59,6 +72,26 @@ func calculate(arr1 *[MAX][MAX]int64, arr2 *[MAX][MAX]int64, arr3 *[MAX][MAX]int
 				arr3[i][j] = temp
 			}
 		}(i)
+	}
+	wg.Wait()
+	return start
+}
+
+func calculate_jik(arr1 *[MAX][MAX]int64, arr2 *[MAX][MAX]int64, arr3 *[MAX][MAX]int64) time.Time {
+	start := time.Now()
+	var wg sync.WaitGroup
+	wg.Add(MAX)
+	for j := 0; j < MAX; j++ {
+		go func(j int) {
+			defer wg.Done()
+			for i := 0; i < MAX; i++ {
+				var temp int64 = 0
+				for k := 0; k < MAX; k++ {
+					temp += power(arr1[i][k], arr2[k][j])
+				}
+				arr3[i][j] = temp
+			}
+		}(j)
 	}
 	wg.Wait()
 	return start
